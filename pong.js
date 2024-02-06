77476fe6f5c9b5247a6c4d50a6372c7b7c88aae8
oneloveoneheart <gahetaflorent@gmail.com>
2024-02-06 19:31:53 +0100

    I added ball_bounce() method and getBoundingClientRect() to see body rect properties

--- pong.js
+++ pong.js
@@ -15,9 +15,34 @@ function placeObjects(objects) {
 }
 
 function update() {
-    ball.x += ball.vx;
-    ball.y += ball.vy;
+    // ball.x += ball.vx;
+    // ball.y += ball.vy;
     placeObjects([ball]);
+    ball_bounce(ball);
+
+    let ballElement = document.getElementById("ball");
+    let ballProperties = ballElement.getBoundingClientRect();
+    console.log(ballProperties);
+
+    let leftPaddle = document.getElementById("left-paddle");
+    console.log(leftPaddle.getBoundingClientRect());
+
+    let rightPaddle = document.getElementById("right-paddle");
+    console.log(rightPaddle.getBoundingClientRect());
+}
+
+function ball_bounce(ball){
+    let bodyElement = document.querySelector("body");
+    let bodyProperties = bodyElement.getBoundingClientRect();
+    console.log(bodyProperties);
+
+    if (ball.x < 0 || ball.x > bodyProperties.width){
+        ball.vx *= -1;
+    }
+
+    if (ball.y < 0 || ball.y > bodyProperties.height){
+        ball.vy *= -1;
+    }
 }
 
 let ball;
@@ -27,7 +52,8 @@ function init() {
 
     // Ensure the DOM is fully loaded before initializing
     document.addEventListener("DOMContentLoaded", function () {
-        setInterval(update, 100);
+        //setInterval(update, 100);
+        update();
     });
 }
 
